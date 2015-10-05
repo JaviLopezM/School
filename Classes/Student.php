617472d722c926d5e9b1c821639b21a35bb75f98
Javi <javierlopez1@iesebre.com>
2015-10-05 19:06:20 +0200

    Creem el nou projecte School.

---
+++ Classes/Student.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: javi
+ * Date: 5/10/15
+ * Time: 17:50
+ */
+class Student extends Worker Trait
+{
+
+    public $classRoomGroup;
+
+}
---
+++ Classes/Worker.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: javi
+ * Date: 5/10/15
+ * Time: 18:26
+ */
+class Worker extends Person
+{
+    public $salary;
+}
